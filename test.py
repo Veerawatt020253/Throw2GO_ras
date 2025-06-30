9d56932d601bb05e8cc6f7584f8b7cd1c5a3c347
Veerawatt020253 <53421@yupparaj.ac.th>
2025-06-30 21:44:27 +0700

    2

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import keyboard  # ใช้ library นี้เพื่ออ่านปุ่มจากคีย์บอร์ด
 
-GPIO.setmode(GPIO.BOARD)  # ใช้หมายเลขขาบนบอร์ดจริง
-servo_pin = 11            # ขา 11 บนบอร์ด = GPIO17
+GPIO.setmode(GPIO.BOARD)
+servo_pin = 11  # Physical Pin 11 (ถ้าใช้ BOARD)
+
 GPIO.setup(servo_pin, GPIO.OUT)
-
-pwm = GPIO.PWM(servo_pin, 50)
+pwm = GPIO.PWM(servo_pin, 50)  # 50Hz
 pwm.start(0)
 
 def set_angle(angle):
@@ -17,15 +18,22 @@ def set_angle(angle):
     pwm.ChangeDutyCycle(0)
 
 try:
+    print("กด W เพื่อหมุนไป 85 องศา, กด S เพื่อกลับไป 0 องศา, กด ESC เพื่อออก")
     while True:
-        set_angle(0)
-        sleep(1)
-        set_angle(90)
-        sleep(1)
-        set_angle(180)
-        sleep(1)
+        if keyboard.is_pressed('w'):
+            print("ไปที่ 85 องศา")
+            set_angle(85)
+            sleep(0.3)
+        elif keyboard.is_pressed('s'):
+            print("กลับที่ 0 องศา")
+            set_angle(0)
+            sleep(0.3)
+        elif keyboard.is_pressed('esc'):
+            print("ออกจากโปรแกรม")
+            break
+
 except KeyboardInterrupt:
-    pwm.stop()
-    GPIO.cleanup()
+    pass
 finally:
+    pwm.stop()
     GPIO.cleanup()
